1b65e41e66b3b3be9ad224da9a2e31da7fbe4e83
William Kerr <kroony@trout-slap.com>
2016-11-30 14:08:28 +1100

    added clips and max clips to game config

--- Pistol/Game_Config.ino
+++ Pistol/Game_Config.ino
@@ -3,16 +3,21 @@ void configureGame() { // Where the game characteristics are stored, allows seve
     myWeaponID = 1;
     maxAmmo = 10;
     ammo = 10;
+    maxAmmoClips = 5;
+    clips = 5;
     maxLife = 3;
     life = 3;
     myWeaponHP = 1;
   }
   if(myGameID == 1){
     myWeaponID = 1;
-    maxAmmo = 100;
-    ammo = 100;
+    maxAmmo = 30;
+    ammo = 30;
+    maxAmmoClips = 3;
+    clips = 3;
     maxLife = 10;
     life = 10;
     myWeaponHP = 2;
   }
 }
+
