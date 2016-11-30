11c05b498e9b23a534409bc48c4b29a494b86514
William Kerr <kroony@trout-slap.com>
2016-11-30 15:53:22 +1100

    Added Reload function

--- Pistol/Pistol.ino
+++ Pistol/Pistol.ino
@@ -19,7 +19,7 @@
 
 // Digital IO's
 int triggerPin             = 3;      // Push button for primary fire. Low = pressed
-int trigger2Pin            = 13;     // Push button for secondary fire. Low = pressed
+int reloadPin              = 13;     // Push button for reload function. Low = pressed
 int speakerPin             = 4;      // Direct output to piezo sounder/speaker
 int audioPin               = 9;      // Audio Trigger. Can be used to set off sounds recorded in the kind of electronics you can get in greetings card that play a custom message.
 int lifePin                = 6;      // An analogue output (PWM) level corresponds to remaining life. Use PWM pin: 3,5,6,9,10 or 11. Can be used to drive LED bar graphs. eg LM3914N
@@ -32,35 +32,38 @@ int IRreceive2Pin          = 11;     // Allows for checking external sensors are
 // Minimum gun requirements: trigger, receiver, IR led, hit LED.
 
 // Player and Game details
-int myTeamID               = 1;      // 1-7 (0 = system message)
-int myPlayerID             = 5;      // Player ID
-int myGameID               = 0;      // Interprited by configureGane subroutine; allows for quick change of game types.
-int myWeaponID             = 0;      // Deffined by gameType and configureGame subroutine.
-int myWeaponHP             = 0;      // Deffined by gameType and configureGame subroutine.
-int maxAmmo                = 0;      // Deffined by gameType and configureGame subroutine.
-int maxLife                = 0;      // Deffined by gameType and configureGame subroutine.
-int automatic              = 0;      // Deffined by gameType and configureGame subroutine. Automatic fire 0 = Semi Auto, 1 = Fully Auto.
-int automatic2             = 0;      // Deffined by gameType and configureGame subroutine. Secondary fire auto?
+byte myTeamID               = 1;      // 1-7 (0 = system message)
+byte myPlayerID             = 2;      // Player ID
+byte myGameID               = 0;      // Interprited by configureGane subroutine; allows for quick change of game types.
+byte myWeaponID             = 0;      // Deffined by gameType and configureGame subroutine.
+byte myWeaponHP             = 0;      // Deffined by gameType and configureGame subroutine.
+byte maxAmmo                = 0;      // Deffined by gameType and configureGame subroutine.
+byte maxAmmoClips           = 0;      // Deffined by gameType and configureGame subroutine.
+byte maxLife                = 0;      // Deffined by gameType and configureGame subroutine.
+byte automatic              = 0;      // Deffined by gameType and configureGame subroutine. Automatic fire 0 = Semi Auto, 1 = Fully Auto.
+byte automatic2             = 0;      // Deffined by gameType and configureGame subroutine. Secondary fire auto?
 
 //Incoming signal Details
 int received[18];                    // Received data: received[0] = which sensor, received[1] - [17] byte1 byte2 parity (Miles Tag structure)
 int check                  = 0;      // Variable used in parity checking
 
 // Stats
-int ammo                   = 0;      // Current ammunition
-int life                   = 0;      // Current life
+byte ammo                   = 0;      // Current ammunition
+byte clips                  = 0;
+byte life                   = 0;      // Current life
 
 // Code Variables
-int timeOut                = 0;      // Deffined in frequencyCalculations (IRpulse + 50)
-int FIRE                   = 0;      // 0 = don't fire, 1 = Primary Fire, 2 = Secondary Fire
-int TR                     = 0;      // Trigger Reading
-int LTR                    = 0;      // Last Trigger Reading
-int T2R                    = 0;      // Trigger 2 Reading (For secondary fire)
-int LT2R                   = 0;      // Last Trigger 2 Reading (For secondary fire)
+int timeOut                 = 0;      // Deffined in frequencyCalculations (IRpulse + 50)
+byte FIRE                   = 0;      // 0 = don't fire, 1 = Primary Fire, 2 = Secondary Fire
+byte RELOAD                 = 0;      // 0 = dont reload, 1 = reload
+byte triggerRead            = 0;      // Trigger Reading
+byte lastTriggerRead        = 0;      // Last Trigger Reading
+byte reloadRead             = 0;      // Trigger 2 Reading (For reload)
+byte lastReloadRead         = 0;      // Last Trigger 2 Reading (For secondary fire)
 
 // Signal Properties
 int IRpulse                = 600;    // Basic pulse duration of 600uS MilesTag standard 4*IRpulse for header bit, 2*IRpulse for 1, 1*IRpulse for 0.
-int IRfrequency            = 38;     // Frequency in kHz Standard values are: 38kHz, 40kHz. Choose dependant on your receiver characteristics
+int IRfrequency            = 36;     // Frequency in kHz Standard values are: 38kHz, 40kHz. Choose dependant on your receiver characteristics
 int IRt                    = 0;      // LED on time to give correct transmission frequency, calculated in setup.
 int IRpulses               = 0;      // Number of oscillations needed to make a full IRpulse, calculated in setup.
 int header                 = 4;      // Header lenght in pulses. 4 = Miles tag standard
@@ -90,7 +93,7 @@ void setup() {
   Serial.println("Startup...");
   // Pin declarations
   pinMode(triggerPin, INPUT);
-  pinMode(trigger2Pin, INPUT);
+  pinMode(reloadPin, INPUT);
   pinMode(speakerPin, OUTPUT);
   pinMode(audioPin, OUTPUT);
   pinMode(lifePin, OUTPUT);
@@ -106,36 +109,38 @@ void setup() {
   tagCode();                 // Based on game details etc works out the data that will be transmitted when a shot is fired
  
  
-  digitalWrite(triggerPin, HIGH);      // Not really needed if your circuit has the correct pull up resistors already but doesn't harm
-  digitalWrite(trigger2Pin, HIGH);     // Not really needed if your circuit has the correct pull up resistors already but doesn't harm
+  //digitalWrite(triggerPin, HIGH);      // Not really needed if your circuit has the correct pull up resistors already but doesn't harm
+  //digitalWrite(reloadPin, HIGH);     // Not really needed if your circuit has the correct pull up resistors already but doesn't harm
  
   for (int i = 1;i < 254;i++) { // Loop plays start up noise
-    analogWrite(ammoPin, i);
+    //analogWrite(ammoPin, i); //light ammo lights in test
     playTone((3000-9*i), 2);
   } 
  
   // Next 4 lines initialise the display LEDs
-  analogWrite(ammoPin, ((int) ammo));
-  analogWrite(lifePin, ((int) life));
-  lifeDisplay();
-  ammoDisplay();
+  //analogWrite(ammoPin, ((int) ammo));
+  //analogWrite(lifePin, ((int) life));
+  //lifeDisplay();
+  //ammoDisplay();
 
-  Serial.print("Ready....Ammo: ");
+  //output gun data
+  Serial.print("Team: ");           Serial.println(myTeamID);
+  Serial.print("Player: ");         Serial.println(myPlayerID);
+  Serial.print("myWeaponHP: ");     Serial.println(myWeaponHP);
+  Serial.print("maxAmmo: ");        Serial.println(maxAmmo);
+  Serial.print("maxAmmoClips: ");   Serial.println(maxAmmoClips);
+  Serial.print("maxLife: ");        Serial.println(maxLife);
+  Serial.println();
+  Serial.print("Ammo: ");
   Serial.print(ammo);
+  Serial.print(" Clips: ");
+  Serial.print(clips);
   Serial.print(" HP: ");
   Serial.println(life);
+  Serial.println("Ready....");
+  Serial.println();
 
-  //delay(500);  hit();
-  delay(500);
-  playAmmo();
-  FIRE = 1; triggers(); shoot(); delay(200);
-  FIRE = 1; triggers(); shoot(); delay(200);
-  playAmmo();
-  FIRE = 1; triggers(); shoot(); delay(200);
-  FIRE = 1; triggers(); shoot(); delay(200);
-  FIRE = 1; triggers(); shoot(); delay(200);
-  FIRE = 1; triggers(); shoot(); delay(200);
-  playAmmo();
+  //delay(500);
   FIRE = 1; triggers(); shoot(); delay(200);
   FIRE = 1; triggers(); shoot(); delay(200);
   FIRE = 1; triggers(); shoot(); delay(200);
@@ -143,16 +148,23 @@ void setup() {
   FIRE = 1; triggers(); shoot(); delay(200);
   FIRE = 1; triggers(); shoot(); delay(200);
   FIRE = 1; triggers(); shoot(); delay(200);
-  
-  
+  FIRE = 1; triggers(); shoot(); delay(200);
+  FIRE = 1; triggers(); shoot(); delay(200);
+  FIRE = 1; triggers(); shoot(); delay(200);
+  FIRE = 1; triggers(); shoot(); delay(200);
+  FIRE = 1; triggers(); shoot(); delay(200);
+  FIRE = 1; triggers(); shoot(); delay(200);
 }
 
 // Main loop most of the code is in the sub routines
 void loop(){
-  receiveIR();
+  //receiveIR();
   if(FIRE != 0){
     shoot();
-    ammoDisplay();
+    //ammoDisplay();//commented as there are no lights
+  }else if(RELOAD !=0){
+    reload();
+    //ammoDisplay();//commented as there are no lights
   }
   triggers();
 }
@@ -160,26 +172,28 @@ void loop(){
 
 // SUB ROUTINES
 
-
-void ammoDisplay() { // Updates Ammo LED output
-  float ammoF;
-  ammoF = (260/maxAmmo) * ammo;
-  if(ammoF <= 0){ammoF = 0;}
-  if(ammoF > 255){ammoF = 255;}
-  analogWrite(ammoPin, ((int) ammoF));
+void reload(){
+  if(RELOAD == 1){
+    if(clips < 1){
+      //play out of clips error
+      noClips();
+    } else {
+      ammo = maxAmmo;
+      clips = clips - 1;
+      Serial.print("Reloading... Ammo: ");
+      Serial.print(ammo);
+      Serial.print(" / Clips: ");
+      Serial.println(clips);
+      playAmmo();
+    }
+  }
+  RELOAD = 0;
+  FIRE = 0;
 }
-
-void lifeDisplay() { // Updates Ammo LED output
-  float lifeF;
-  lifeF = (260/maxLife) * life;
-  if(lifeF <= 0){lifeF = 0;}
-  if(lifeF > 255){lifeF = 255;}
-  analogWrite(lifePin, ((int) lifeF));
-} 
 
 void shoot() {
   if(FIRE == 1){ // Has the trigger been pressed?
-    Serial.println("FIRE 1");
+    Serial.print("Shooting...");
     sendPulse(IRtransmitPin, 4); // Transmit Header pulse, send pulse subroutine deals with the details
     delayMicroseconds(IRpulse);
  
@@ -208,45 +222,56 @@ void shoot() {
     }
     sendPulse(IRtransmitPin, 1);
     delayMicroseconds(IRpulse);
-    Serial.print("DONE 1 Ammo: ");
-    Serial.println(ammo);
+    Serial.print("success");
     playGunShot();//buzzerSound
+    ammo = ammo - 1;
+    Serial.print(" Ammo:");
+    Serial.println(ammo);
+
   }
 
-  if(FIRE == 2){ // Where a secondary fire mode would be added
+  /*if(FIRE == 2){ // Where a secondary fire mode would be added
     Serial.println("FIRE 2");
     sendPulse(IRtransmitPin, 4); // Header
     Serial.println("DONE 2");
-  }
-FIRE = 0;
-ammo = ammo - 1;
-if(ammo < 0){ammo=0;}//stop ammo going negative
-
+  }*/
+  
+  RELOAD = 0;
+  FIRE = 0;
 }
 
 void triggers() { // Checks to see if the triggers have been presses
-  LTR = TR;       // Records previous state. Primary fire
-  LT2R = T2R;     // Records previous state. Secondary fire
-  TR = digitalRead(triggerPin);      // Looks up current trigger button state
-  T2R = digitalRead(trigger2Pin);    // Looks up current trigger button state
+  
+  //Trigger
+  lastTriggerRead = triggerRead;       // Records previous state. trigger button
+  triggerRead = digitalRead(triggerPin);      // Looks up current trigger button state
   // Code looks for changes in trigger state to give it a semi automatic shooting behaviour
-  if(TR != LTR && TR == LOW){
+  if(triggerRead != lastTriggerRead && triggerRead == LOW){
     FIRE = 1;
-    Serial.println("T1 press");
+    Serial.println("Trigger press");
   }
-  if(T2R != LT2R && T2R == LOW){
-    FIRE = 2;
-  }
-  if(TR == LOW && automatic == 1){
+  /*if(TR == LOW && automatic == 1){
     FIRE = 1;
   }
   if(T2R == LOW && automatic2 == 1){
     FIRE = 2;
-  }
+  }*/
   if(FIRE == 1 || FIRE == 2){
     if(life < 1){FIRE = 0; dead();}
     if(ammo < 1){FIRE = 0; noAmmo();}
     // Fire rate code to be added here  
+  }
+
+  //Reload
+  lastReloadRead = reloadRead;     // Records previous state. reload button
+  reloadRead = digitalRead(reloadPin);    // Looks up current reload button state
+  if(reloadRead != lastReloadRead && reloadRead == LOW){
+    RELOAD = 1;
+    Serial.println("Reload press");
+  }
+  if(RELOAD == 1){
+    if(life < 1){RELOAD = 0; dead();}
+    if(clips < 1){RELOAD = 0; noClips();}
   }
  
 }
@@ -271,13 +296,24 @@ void dead() { // void determines what the tagger does when it is out of lives
   }
 }
 
+void noClips() { // Make some noise and flash some lights when out of clips
+  digitalWrite(hitPin,HIGH);
+  playNoClips();
+  digitalWrite(hitPin,LOW);
+  Serial.print("OUT OF CLIPS... Ammo: ");
+  Serial.print(ammo);
+  Serial.print(" / Clips:");
+  Serial.println(clips);
+}
 
 void noAmmo() { // Make some noise and flash some lights when out of ammo
   digitalWrite(hitPin,HIGH);
   playNoAmmo();
   digitalWrite(hitPin,LOW);
-  Serial.print("Out of ammo: ");
-  Serial.println(ammo);
+  Serial.print("OUT OF AMMO... Ammo: ");
+  Serial.print(ammo);
+  Serial.print(" / Clips:");
+  Serial.println(clips);
 }
 
 
@@ -291,4 +327,22 @@ void hit() { // Make some noise and flash some lights when you get shot
   digitalWrite(hitPin,LOW);
   lifeDisplay();
 }
+
+
+
+void ammoDisplay() { // Updates Ammo LED output
+  float ammoF;
+  ammoF = (260/maxAmmo) * ammo;
+  if(ammoF <= 0){ammoF = 0;}
+  if(ammoF > 255){ammoF = 255;}
+  analogWrite(ammoPin, ((int) ammoF));
+}
+
+void lifeDisplay() { // Updates Ammo LED output
+  float lifeF;
+  lifeF = (260/maxLife) * life;
+  if(lifeF <= 0){lifeF = 0;}
+  if(lifeF > 255){lifeF = 255;}
+  analogWrite(lifePin, ((int) lifeF));
+} 
 
