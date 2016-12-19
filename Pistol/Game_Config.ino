c424e2b196a3ed02c8e5a99ac7f9bca924df789d
William Kerr <kroony@trout-slap.com>
2016-12-19 14:34:54 +1100

    renamed varibles to HP and Damage

--- Pistol/Game_Config.ino
+++ Pistol/Game_Config.ino
@@ -1,23 +1,24 @@
 void configureGame() { // Where the game characteristics are stored, allows several game types to be recorded and you only have to change one variable (myGameID) to pick the game.
-  if(myGameID == 0){
+  if(myGameID == 0){//pistol
     myWeaponID = 1;
     maxAmmo = 10;
     ammo = 10;
     maxAmmoClips = 5;
     clips = 5;
-    maxLife = 3;
-    life = 3;
-    myWeaponHP = 1;
+    maxHp = 3;
+    hp = 3;
+    myWeaponDamage = 1;
   }
-  if(myGameID == 1){
+  
+  if(myGameID == 1){//Semi Auto AR
     myWeaponID = 1;
     maxAmmo = 30;
     ammo = 30;
     maxAmmoClips = 3;
     clips = 3;
-    maxLife = 10;
-    life = 10;
-    myWeaponHP = 2;
+    maxHp = 10;
+    hp = 10;
+    myWeaponDamage = 2;
   }
 }
 
